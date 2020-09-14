e73925c8afc2f8dcfd351dfc0846e9a4fe064384
qeOnda <qeOnda@users.noreply.github.com>
2020-09-13 20:33:50 -0500

    set get request with axios

--- frontend/src/Views/Tricks.js
+++ frontend/src/Views/Tricks.js
@@ -2,23 +2,39 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Tricks() {
-	state = {
-		tricks: []
-	}
+	const [tricks, setTricks] = useState([]);
+	const [load, setLoad] = useState(false);
+	const [error, setError] = useState('');
 
-	componentDidMount() {
-		axios.get("http://localhost:8000/api/tricks/")
-			.then(res => {
-				const tricks = res.data
-				this.setState({ tricks });
+	useEffect(() => {
+		axios.get('http://localhost:8000/api/tricks/?format=json')
+			.then(response => {
+				setTricks(response.data);
+				setLoad(true);
 			})
+			.catch(error => {
+				setError('Something went wrong.');
+				setLoad(true)
+			})
+	}, []);
+
+	if(load) {
+		return (
+			<ul>
+				{
+					error ? <li>{error}</li>:
+					tricks.map((trick, index) => 
+						<li key={index}>Name:{trick.name}</li>		
+				)}
+			</ul>
+		);
+	} else {
+		return (
+			<div>
+				Loading...
+			</div>
+		);
 	}
+};
 
-	return (
-		<ul>
-			{ this.state.tricks.map(tricks => <li>{tricks.name}</li>)}
-		</ul>
-	)
-}
-
-export default Tricksx
+export default Tricks;
